86722923d27ff16fd618b0774cc0c842842cbf32
Andrey Subbotin <andrey@subbotin.me>
2021-02-05 10:57:29 +1100

    Refactor things a little
    Signed-off-by: Andrey Subbotin <andrey@subbotin.me>

--- src/eploko/globe3.clj
+++ src/eploko/globe3.clj
@@ -10,9 +10,9 @@
 (def will-restart-subj ::will-restart)
 (def will-start-subj ::will-start)
 
-(defn msg
+(defn mk-msg
   ([subj]
-   (msg subj nil))
+   (mk-msg subj nil))
   ([subj body]
    {:subj subj
     :body body}))
@@ -25,52 +25,72 @@
   [msg]
   (:body msg))
 
+(defn- mk-actor
+  [role]
+  {:role role
+   :msg-port (chan)})
+
+(defn- get-actor-role
+  [actor]
+  (:role actor))
+
+(defn- get-actor-msg-port
+  [actor]
+  (:msg-port actor))
+
+(defn send!
+  [actor msg]
+  (go (>! (get-actor-msg-port actor) msg)))
+
 (defn- unknown-msg-h
-  [_state _payload]
-  (println "Not handled."))
+  [state _body]
+  (println "Not handled.")
+  state)
 
 (defn- resolve-handler
-  [actor subj]
-  (let [handlers (actor)]
+  [role subj]
+  (let [handlers (role)]
     (or (get handlers subj)
         (get handlers proxy-subj)
         #'unknown-msg-h)))
 
 (defn- run-actor!
   [actor]
-  (let [in-port (chan)]
-      (go-loop [state nil]
-        (if-let [msg (<! in-port)]
-          (let [subj (msg-subj msg)
-                h (resolve-handler actor subj)
-                body (msg-body msg)]
-            (recur (h state body)))
-          (prn "terminated")))
-      in-port))
+  (let [role (get-actor-role actor)
+        msg-port (get-actor-msg-port actor)]
+    (go-loop [state nil]
+      (if-let [msg (<! msg-port)]
+        (let [subj (msg-subj msg)
+              body (msg-body msg)
+              h (resolve-handler role subj)
+              new-state (h state body)]
+          (recur new-state))
+        (prn "terminated")))))
 
 (defn spawn!
-  ([actor]
-   (spawn! actor {}))
-  ([actor props]
-   (let [actor-port (run-actor! actor)]
-     (go (>! actor-port (msg init-subj props))
-         (>! actor-port (msg will-start-subj)))
-     actor-port)))
+  ([role]
+   (spawn! role {}))
+  ([role props]
+   (let [actor (mk-actor role)]
+     (run-actor! actor)
+     (go (send! actor (mk-msg init-subj props))
+         (send! actor (mk-msg will-start-subj)))
+     actor)))
 
 (defn stop!
-  [addr]
-  (go (>! addr (msg did-stop-subj))
-      (async/close! addr)))
+  [actor]
+  (go (send! actor (mk-msg did-stop-subj))
+      (async/close! (get-actor-msg-port actor))))
 
 (defn restart!
-  [addr]
-  (go (>! addr (msg will-restart-subj))
-      (>! addr (msg did-stop-subj))
+  [actor]
+  (go (send! actor (mk-msg will-restart-subj))
+      (send! actor (mk-msg did-stop-subj))
       ;; reset state somehow
       ;; get props somehow
-      #_(>! addr (msg init-subj props))
-      (>! addr (msg will-start-subj))
-      (>! addr (msg did-restart-subj))))
+      #_(send! actor (mk-msg init-subj props))
+      (send! actor (mk-msg will-start-subj))
+      (send! actor (mk-msg did-restart-subj))))
 
 (comment
   (defn init-h
@@ -79,7 +99,8 @@
   
   (defn greet-h
     [state body]
-    (println (str (:greeting state) " " body "!")))
+    (println (str (:greeting state) " " body "!"))
+    state)
   
   (defn greeter
     []
@@ -87,10 +108,10 @@
      :greet #'greet-h})
 
   (def greeter-addr (spawn! #'greeter {:greeting "Hej"}))
-  (go (>! greeter-addr (msg :greet "Andrey")))
+  (send! greeter-addr (msg :greet "Andrey"))
   (stop! greeter-addr)
   (restart! greeter-addr)
 
-  (ns-unmap (find-ns 'eploko.globe3) 'msg-payload)
+  (ns-unmap (find-ns 'eploko.globe3) 'msg)
   ,)
 
