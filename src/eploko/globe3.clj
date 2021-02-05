bf92c4f52290bd32718fd9f9d4424f2c38a39ad6
Andrey Subbotin <andrey@subbotin.me>
2021-02-05 21:26:18 +1100

    Simplify things heaps
    Signed-off-by: Andrey Subbotin <andrey@subbotin.me>

--- src/eploko/globe3.clj
+++ src/eploko/globe3.clj
@@ -7,9 +7,6 @@
 (def did-stop-subj ::did-stop)
 (def init-subj ::init)
 (def proxy-subj ::proxy)
-(def ^:private reset-state-subj ::reset-state)
-(def ^:private set-delivery-mode-subj ::set-delivery-mode)
-(def ^:private terminate-subj ::terminate)
 (def will-restart-subj ::will-restart)
 (def will-start-subj ::will-start)
 
@@ -30,51 +27,11 @@
   [msg]
   (:body msg))
 
-(defn- emsg
-  [mode body]
-  [mode body])
-
-(defn- toggle-switched-chan-ports
-  "Switches the mix to be a solo of the channel under `solo-k`."
-  [mix ports-map solo-k]
-  (println "switching mix to mode" solo-k)
-  (async/toggle mix
-                (reduce-kv (fn [accu k v] (assoc accu v {:pause (not= k solo-k)}))
-                           {}
-                           ports-map)))
-
-(defn switched-chan
-  "Creates a channel switching between channels in `ports-map`.
-  `ports-map` is a map of keys to channels. You put keys onto
-  the `ctrl-port` to switch the resulting channel. The resulting
-  channel is closed when `ctrl-port` closes. Initially every
-  channel in `ports-map` is paused."
-  [ctrl-port ports-map]
-  (let [out-port (chan)
-        mix (async/mix out-port)]
-    (toggle-switched-chan-ports mix ports-map nil)
-    (go-loop []
-      (if-some [k (<! ctrl-port)]
-        (do
-          (toggle-switched-chan-ports mix ports-map k)
-          (recur))
-        (async/close! out-port)))
-    out-port))
-
-(defn- mk-delivery-ports
-  [mailbox-size]
-  {:normal (chan mailbox-size)
-   :oob (chan mailbox-size)})
-
 (defn- mk-actor
   [role props]
-  (let [delivery-ports (mk-delivery-ports mailbox-size)
-        runner-feed-ctrl-port (chan)]
-    {:role role
-     :props props
-     :delivery-ports delivery-ports
-     :runner-feed-ctrl-port runner-feed-ctrl-port
-     :runner-feed (switched-chan runner-feed-ctrl-port delivery-ports)}))
+  {:role role
+   :props props
+   :port (chan mailbox-size)})
 
 (defn- get-actor-role
   [actor]
@@ -84,35 +41,12 @@
   [actor]
   (:props actor))
 
-(defn- get-actor-delivery-port
-  [actor delivery-mode]
-  (get-in actor [:delivery-ports delivery-mode]))
-
-(defn- get-actor-runner-feed
+(defn- get-actor-port
   [actor]
-  (:runner-feed actor))
-
-(defn- get-actor-runner-feed-ctrl-port
-  [actor]
-  (:runner-feed-ctrl-port actor))
-
-(defn- set-actor-delivery-mode!
-  [actor delivery-mode]
-  (go (>! (get-actor-runner-feed-ctrl-port actor)
-          delivery-mode)))
-
-(defn send!
-  [actor msg & {:as opts}]
-  (go (>! (get-actor-delivery-port actor (get opts :delivery-mode :normal))
-          (emsg (get opts :emsg-mode :normal) msg))))
-
-(defn- ctx-actor
-  [ctx]
-  (:actor ctx))
+  (:port actor))
 
 (defn- unknown-msg-h
-  [_ctx state _body]
-  (println "Not handled.")
+  [state _body]
   state)
 
 (defn- resolve-handler
@@ -122,44 +56,27 @@
         (get handlers proxy-subj)
         #'unknown-msg-h)))
 
-(defn- terminate-syscall
-  [ctx _state _body]
-  (let [actor (ctx-actor ctx)]
-    (async/close! (get-actor-delivery-port actor :normal))
-    (async/close! (get-actor-delivery-port actor :oob))
-    (async/close! (get-actor-runner-feed-ctrl-port actor))
-    (prn "terminated")))
+(defn enqueu-op!
+  [actor op-f]
+  (go (>! (get-actor-port actor) op-f)))
 
-(defn- set-delivery-mode-syscall
-  [ctx state body]
-  (set-actor-delivery-mode! (ctx-actor ctx) body)
-  state)
+(defn- msg-op
+  [actor msg state]
+  (let [subj (msg-subj msg)
+        body (msg-body msg)
+        role (get-actor-role actor)
+        h (resolve-handler role subj)]
+    (h state body)))
 
-(defn- reset-state-syscall
-  [_ctx _state _body]
-  nil)
-
-(defn- syscall-role
-  []
-  {reset-state-subj #'reset-state-syscall
-   set-delivery-mode-subj #'set-delivery-mode-syscall
-   terminate-subj #'terminate-syscall})
+(defn send!
+  [actor msg]
+  (enqueu-op! actor (partial msg-op actor msg)))
 
 (defn- run-actor!
   [actor]
-  (let [role (get-actor-role actor)
-        feed (get-actor-runner-feed actor)]
-    (go-loop [state nil]
-      (when-some [[mode msg] (<! feed)]
-        (println ">>" mode msg)
-        (let [subj (msg-subj msg)
-              body (msg-body msg)
-              target-role (case mode
-                            :normal role
-                            :syscall #'syscall-role)
-              h (resolve-handler target-role subj)
-              ctx {:actor actor}]
-          (recur (h ctx state body)))))))
+  (go-loop [state nil]
+    (when-some [op-f (<! (get-actor-port actor))]
+      (recur (op-f state)))))
 
 (defn spawn!
   ([role]
@@ -167,40 +84,40 @@
   ([role props]
    (let [actor (mk-actor role props)]
      (run-actor! actor)
-     (set-actor-delivery-mode! actor :oob)
-     (send! actor (mk-msg init-subj props) :delivery-mode :oob)
-     (send! actor (mk-msg will-start-subj) :delivery-mode :oob)
-     (send! actor (mk-msg set-delivery-mode-subj :normal)
-            :delivery-mode :oob :emsg-mode :syscall)
+     (enqueu-op! actor
+                 (fn [state]
+                   (->> state
+                        (msg-op actor (mk-msg init-subj props))
+                        (msg-op actor (mk-msg will-start-subj)))))
      actor)))
 
 (defn stop!
   [actor]
-  (set-actor-delivery-mode! actor :oob)
-  (send! actor (mk-msg did-stop-subj) :delivery-mode :oob)
-  (send! actor (mk-msg terminate-subj) :delivery-mode :oob :emsg-mode :syscall))
+  (enqueu-op! actor
+              (fn [state]
+                (msg-op actor (mk-msg did-stop-subj) state)
+                (async/close! (get-actor-port actor)))))
 
 (defn restart!
   [actor]
-  (set-actor-delivery-mode! actor :oob)
-  (send! actor (mk-msg will-restart-subj) :delivery-mode :oob)
-  (send! actor (mk-msg did-stop-subj) :delivery-mode :oob)
-  (send! actor (mk-msg reset-state-subj)
-         :delivery-mode :oob :emsg-mode :syscall)
-  (send! actor (mk-msg init-subj (get-actor-props actor)) :delivery-mode :oob)
-  (send! actor (mk-msg will-start-subj) :delivery-mode :oob)
-  (send! actor (mk-msg did-restart-subj) :delivery-mode :oob)
-  (send! actor (mk-msg set-delivery-mode-subj :normal)
-            :delivery-mode :oob :emsg-mode :syscall))
+  (enqueu-op! actor
+              (fn [state]
+                (->> state
+                     (msg-op actor (mk-msg will-restart-subj))
+                     (msg-op actor (mk-msg did-stop-subj)))
+                (->> nil
+                     (msg-op actor (mk-msg init-subj (get-actor-props actor)))
+                     (msg-op actor (mk-msg will-start-subj))
+                     (msg-op actor (mk-msg did-restart-subj))))))
 
 (comment
   (defn init-h
-    [_ctx _state body]
+    [_state body]
     (println "init-h" body)
     body)
   
   (defn greet-h
-    [_ctx state body]
+    [state body]
     (println (str (:greeting state) " " body "!"))
     state)
   
